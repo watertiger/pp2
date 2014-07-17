7b50b3151242ac46a7822855867012e70c81edbb
watertiger <darkwkt@gmail.com>
2014-07-17 06:11:19 +0000

    t位数组的定义方式

--- chapter01/bitsort.h
+++ chapter01/bitsort.h
@@ -2,61 +2,76 @@
 #include <stdlib.h>
 #include <time.h>
 
-//方案1和方案2共用
-#define BITCHAR 8
+//采用char乘除法的方案
+//#define BIT_CHAR_MD
+//采用char与或的方案
+//#define BIT_CHAR_AO
+//采用int乘除法的方案
+//#define BIT_INT_MD
+//采用int与或的方案
+#define BIT_int_AO
 
-//位数组方案1（使用乘除法）
-#define BITC_NSLOT(ni) ( (ni + BITCHAR -1) / BITCHAR )
-#define BITC_SLOT(i) ( (i) / BITCHAR )
-#define BITC_MASK(i) ( 1 << ( (i) % BITCHAR ) )
-#define BITC_SET(a,i)	( (a)[BITC_SLOT(i)] |= BITC_MASK(i) )
-#define BITC_CLR(a,i)	( (a)[BITC_SLOT(i)] &= ~BITC_MASK(i) )
-#define BITC_TEST(a,i)	( (a)[BITC_SLOT(i)] & BITC_MASK(i) )
+#ifdef BIT_CHAR_MD
+	//位数组方案1（使用乘除法）
+	#define BITCHAR 8
+	#define BIT_NSLOT(ni) ( (ni + BITCHAR -1) / BITCHAR )
+	#define BIT_SLOT(i) ( (i) / BITCHAR )
+	#define BIT_MASK(i) ( 1 << ( (i) % BITCHAR ) )
+	#define BIT_SET(a,i)	( (a)[BIT_SLOT(i)] |= BIT_MASK(i) )
+	#define BIT_CLR(a,i)	( (a)[BIT_SLOT(i)] &= ~BIT_MASK(i) )
+	#define BIT_TEST(a,i)	( (a)[BIT_SLOT(i)] & BIT_MASK(i) )
+#elif defined BIT_CHAR_AO
+	//位数组方案2（使用移位和与）
+	#define BITCHAR 8
+	#define BITCHAR_SHIFT 3
+	#define CHAR_MASK 0x07
+	#define BIT_NSLOT(ni) ( (ni + BITCHAR -1) >> BITCHAR_SHIFT )
+	#define BIT_SLOT(i) ( (i) >> BITCHAR_SHIFT)
+	#define BIT_MASK(i) ( 1 << ( (i) & CHAR_MASK ) )
+	#define BIT_SET(a,i)	( (a)[BIT_SLOT(i)] |= BIT_MASK(i) )
+	#define BIT_CLR(a,i)	( (a)[BIT_SLOT(i)] &= ~BIT_MASK(i) )
+	#define BIT_TEST(a,i)	( (a)[BIT_SLOT(i)] & BIT_MASK(i) )
+#elif defined BIT_INT_MD	
+	//位数组方案3（使用乘除法）
+	#define BITINT 32
+	#define BIT_NSLOT(ni) ( (ni + BITINT -1) / BITINT )
+	#define BIT_SLOT(i) ( (i) / BITINT )
+	#define BIT_MASK(i) ( 1 << ( (i) % BITINT ) )
+	#define BIT_SET(a,i)	( (a)[BIT_SLOT(i)] |= BIT_MASK(i) )
+	#define BIT_CLR(a,i)	( (a)[BIT_SLOT(i)] &= ~BIT_MASK(i) )
+	#define BIT_TEST(a,i)	( (a)[BIT_SLOT(i)] & BIT_MASK(i) )
+#else
+	//位数组方案4（使用移位和与）
+	#define BITINT 32
+	#define BITINT_SHIFT 5
+	#define INT_MASK 0x1F
+	#define BIT_NSLOT(ni) ( (ni + BITINT -1) >> BITINT_SHIFT )
+	#define BIT_SLOT(i) ( (i) >> BITINT_SHIFT)
+	#define BIT_MASK(i) ( 1 << ( (i) & INT_MASK ) )
+	#define BIT_SET(a,i)	( (a)[BIT_SLOT(i)] |= BIT_MASK(i) )
+	#define BIT_CLR(a,i)	( (a)[BIT_SLOT(i)] &= ~BIT_MASK(i) )
+	#define BIT_TEST(a,i)	( (a)[BIT_SLOT(i)] & BIT_MASK(i) )
+#endif
 
-//位数组方案2（使用移位和与）
-#define BITCHAR_SHIFT 3
-#define CHAR_MASK 0x07
-#define BITCHAR_NSLOT(ni) ( (ni + BITCHAR -1) >> BITCHAR_SHIFT )
-#define BITCHAR_SLOT(i) ( (i) >> BITCHAR_SHIFT)
-#define BITCHAR_MASK(i) ( 1 << ( (i) & CHAR_MASK ) )
-#define BITCHAR_SET(a,i)	( (a)[BITCHAR_SLOT(i)] |= BITCHAR_MASK(i) )
-#define BITCHAR_CLR(a,i)	( (a)[BITCHAR_SLOT(i)] &= ~BITCHAR_MASK(i) )
-#define BITCHAR_TEST(a,i)	( (a)[BITCHAR_SLOT(i)] & BITCHAR_MASK(i) )
-	
-//方案3和方案4共用
-#define BITINT 32
-
-//位数组方案3（使用乘除法）
-#define BITI_NSLOT(ni) ( (ni + BITINT -1) / BITINT )
-#define BITI_SLOT(i) ( (i) / BITINT )
-#define BITI_MASK(i) ( 1 << ( (i) % BITINT ) )
-#define BITI_SET(a,i)	( (a)[BITI_SLOT(i)] |= BITI_MASK(i) )
-#define BITI_CLR(a,i)	( (a)[BITI_SLOT(i)] &= ~BITI_MASK(i) )
-#define BITI_TEST(a,i)	( (a)[BITI_SLOT(i)] & BITI_MASK(i) )
-
-//位数组方案4（使用移位和与）
-#define BITINT_SHIFT 5
-#define INT_MASK 0x1F
-#define BITINT_NSLOT(ni) ( (ni + BITINT -1) >> BITINT_SHIFT )
-#define BITINT_SLOT(i) ( (i) >> BITINT_SHIFT)
-#define BITINT_MASK(i) ( 1 << ( (i) & INT_MASK ) )
-#define BITINT_SET(a,i)	( (a)[BITINT_SLOT(i)] |= BITINT_MASK(i) )
-#define BITINT_CLR(a,i)	( (a)[BITINT_SLOT(i)] &= ~BITINT_MASK(i) )
-#define BITINT_TEST(a,i)	( (a)[BITINT_SLOT(i)] & BITINT_MASK(i) )
-
-void bitc_sort(int *arr, int size, int max)
+void bit_sort(int *arr, int size, int max)
 {
 	int i = 0;
-	char bitarray[BITC_NSLOT(max)];
+	char bitarray[BIT_NSLOT(max)];
 	
 	for(i = 0; i < max; i++)
-		BITC_CLR(bitarray, i);
+		BIT_CLR(bitarray, i);
 	clock_t start, end; //单位是ms
 	start = clock();
 	for( i =0; i < size; i++)
-		BITC_SET(bitarray, arr[i]);
+		BIT_SET(bitarray, arr[i]);
 	end = clock();
-	printf("BITCSORT: %f seconds used.\n", ((double)(end-start))/1000000);
+	printf("BITSORT: %f seconds used.\n", ((double)(end-start))/1000000);
+	start = clock();
+	for( i =0; i < size; i++)
+		if(BIT_TEST(bitarray,i))
+			printf("%07d\n", i);
+	end = clock();
+	printf("BITSORT: %f seconds used.\n", ((double)(end-start))/1000000);
 	
 	return;
 }	
